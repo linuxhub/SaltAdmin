198b071af9948d622ed38e8161a33e6ff69eca6e
Arjit Chauhan <arjitc12@gmail.com>
2019-10-31 15:55:35 +0530

    Update run.py

--- run.py
+++ run.py
@@ -15,6 +15,7 @@ expired_message = '''
 <html lang="zh-CN">
 	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<script Language="JavaScript">alert("会话已过期，请重新登录！"); window.location.href="/login";</script>
 	</head>
 <html>
